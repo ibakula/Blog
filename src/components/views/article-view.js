e63826d85d66d0217f32ed27fd3821e5882fe614
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-07 18:48:28 +0200

    Modified article view's styling

--- src/components/views/article-view.js
+++ src/components/views/article-view.js
@@ -9,7 +9,7 @@ export default function ArticleView(props) {
           <Card.Body>
             <Card.Title><Card.Img src="https://cdn.pixabay.com/photo/2014/05/02/23/46/bridge-336475_960_720.jpg" className={"mr-4 ml-2 " + style.imgRuleset} />Some title..</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">November 1, 2020</Card.Subtitle>
-            <Card.Text className={style.textRuleset}>Added some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results</Card.Text>
+            <Card.Text className={style.textRuleset}>Added some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results...</Card.Text>
             <Card.Link href="/article/5">Read more...</Card.Link>
           </Card.Body>
         </Card>
