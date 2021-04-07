a1cbdea855c8cf1e1db22c8855a39403b563ea24
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-07 18:55:04 +0200

    Small fix

--- src/components/views/popular-articles-view.js
+++ src/components/views/popular-articles-view.js
@@ -13,8 +13,8 @@ export default function PopularArticles(props) {
         <Card border="light">
           <Card.Img className={"card-body pb-0 " + style.limitImgSize} variant="top" src="https://cdn.pixabay.com/photo/2014/05/02/23/46/bridge-336475_960_720.jpg" />
           <Card.Body>
-            <Card.Title class={style.titleBoundaries}>Some long title, veeeeeeeeery loong title</Card.Title>
-            <Card.Text class={style.textBoundaries}>Added some text to see the results, but decided to make the text extremly long so I could see where do I want it to stop and I find text size extremly important. Etcetcetc</Card.Text>
+            <Card.Title className={style.titleBoundaries}>Some long title, veeeeeeeeery loong title</Card.Title>
+            <Card.Text className={style.textBoundaries}>Added some text to see the results, but decided to make the text extremly long so I could see where do I want it to stop and I find text size extremly important. Etcetcetc</Card.Text>
             <Card.Link href="/article/5">Read more...</Card.Link>
           </Card.Body>
         </Card>
