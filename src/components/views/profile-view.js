192a4e4538fa8a8c7eea26e689ee92009a8a6450
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-08 09:44:41 +0200

    profile view styling completion

--- src/components/views/profile-view.js
+++ src/components/views/profile-view.js
@@ -1,25 +1,39 @@
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col } from 'react-bootstrap';
 
 export default function ProfileView(props) {
   return(
     <Form className="mt-sm-5 mt-3 pl-sm-5 pl-3 pr-sm-5 pr-3">
-      <Form.Group controlId="">
-        <Form.Label>First name:</Form.Label>
-        <Form.Control placeholder="First name" />
-      </Form.Group>
-      <Form.Group controlId="">
-        <Form.Label>Last name:</Form.Label>
-        <Form.Control placeholder="Surname" />
-      </Form.Group>
-      <Form.Group controlId="">
-        <Form.Label>E-mail:</Form.Label>
-        <Form.Control placeholder="New e-mail address" />
-      </Form.Group>
-      <Form.Group controlId="">
-        <Form.Label>Permissions</Form.Label>
-        <Form.Control type="text" placeholder="Member" disabled/>
-      </Form.Group>
-      <Button variant="primary">Apply changes!</Button>
+      <Form.Row>
+        <Form.Group as={Col} controlId="regFirstName">
+          <Form.Label>First name:</Form.Label>
+          <Form.Control type="text" placeholder="First name" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="regLastName">
+          <Form.Label>Last name:</Form.Label>
+          <Form.Control type="text" placeholder="Surname" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="regEmail">
+          <Form.Label>E-mail:</Form.Label>
+          <Form.Control type="email" placeholder="New e-mail address" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="regPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="New password" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="regPermissions">
+          <Form.Label>Permissions</Form.Label>
+          <Form.Control type="text" placeholder="Member" disabled/>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Col>
+          <Button variant="primary">Apply changes!</Button>
+        </Col>
+      </Form.Row>
     </Form>
   );
 };
