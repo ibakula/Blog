3348ddd6009c29f49e61df3d0a219eb907ee759a
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-08 21:49:59 +0200

    Added 404 Page and some rules

---
+++ src/components/layouts/404-layout.js
@@ -0,0 +1,8 @@
+export default function NotFound(props) {
+  return (
+    <div style={{height:"25em"}} className="w-100 bg-dark pt-sm-5 pt-3 pb-sm-5 pb-5 text-center">
+      <h3 className="display-4 text-warning">Page not found!</h3>
+      <p className="display-5 text-white">The page you have requested could not be found.</p>
+    </div>
+  );
+}
--- src/components/layouts/main-layout.js
+++ src/components/layouts/main-layout.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // Route-specific layouts
 import Home from '../home';
@@ -12,6 +12,7 @@ import Login from './login-layout';
 import Registration from './registration-layout';
 import Profile from './profile-layout';
 import Article from './article-layout';
+import NotFound from './404-layout';
 
 export default class MainLayout extends Component {
   static propTypes = {  
@@ -23,11 +24,19 @@ export default class MainLayout extends Component {
         <NavigationBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Registration} />
-          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/login" component={Login}>
+            {localStorage.getItem("email") != null && <Redirect to="/404" />}
+          </Route>
+          <Route exact path="/register" component={Registration}>
+            {localStorage.getItem("email") != null && <Redirect to="/404" />}
+          </Route>
+          <Route exact path="/profile" component={Profile}>
+            {localStorage.getItem("email") != null && <Redirect to="/404" />}
+          </Route>
+          <Route exact path="/404" component={NotFound} />
           <Route path="/profile/:userId" component={Profile}/>
           <Route path="/article/:articleId" component={Article} />
+          <Route path="*" component={NotFound} />
         </Switch>
         <Footer />
       </>
