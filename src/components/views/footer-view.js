316bc3643e36bd2b22985da9656ffe811ef7eab7
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-08 21:36:16 +0200

    Footer styling mod

--- src/components/views/footer-view.js
+++ src/components/views/footer-view.js
@@ -4,7 +4,7 @@ import style from '../linksinheritdeco.module.css';
 export default class FooterView extends Component {
   render() {
     return (
-      <footer class="bg-light display-block text-center p-3 mt-5"><a href="#navbar" class={style.removeDecorations}>Go to the top!</a></footer>
+      <footer class="bg-light display-block text-center p-3"><a href="#navbar" class={style.removeDecorations}>Go to the top!</a></footer>
     );
   }
 };
