b4b682537762ee6175e254181f2bf0af5cceec97
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-09 10:57:38 +0200

    Small correction on the application of a class name

--- src/components/views/article-view.js
+++ src/components/views/article-view.js
@@ -11,7 +11,7 @@ export default function ArticleView(props) {
             <Card.Title><Card.Img src="https://cdn.pixabay.com/photo/2014/05/02/23/46/bridge-336475_960_720.jpg" className={"mr-4 ml-2 " + style.imgRuleset} />Some title..</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">November 1, 2020</Card.Subtitle>
             <Card.Text className={style.textRuleset}>Added some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results some text to see the results...</Card.Text>
-            <Link to="/article/5" class="card-link">Read more...</Link>
+            <Link to="/article/5" className="card-link">Read more...</Link>
           </Card.Body>
         </Card>
       </Col>
