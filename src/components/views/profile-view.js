1d4f19743c3259f20435a477a48056c071428e39
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-10 08:07:47 +0200

    Small styling changes to the profile view

--- src/components/views/profile-view.js
+++ src/components/views/profile-view.js
@@ -2,19 +2,6 @@ import { Form, Button, Col } from 'react-bootstrap';
 import LoaderStripe from './loader-stripe-view';
 import style from './profile.module.css';
 
-function displayPermissions(permissionId) {
-  switch (permissionId) {
-    case 1:
-      return "Author";
-    case 2:
-      return "Moderator";
-    case 3:
-      return "Administrator";
-  }
-
-  return "Member";
-}
-
 export default function ProfileView(props) {
   let date = null;
   if (props.userData != null && 'signup_date' in props.userData) {
@@ -24,8 +11,8 @@ export default function ProfileView(props) {
   const userProvided = ('userId' in props && props.userId != null ? 
   (<div className={`w-100 pt-sm-5 pt-3 pb-sm-5 pb-3 pl-sm-5 pl-3 pr-sm-5 pr-3 ${style.bgDark}`}>
     {props.userData == null || props.userData.id != props.userId ? 
-    <><LoaderStripe widthMultiplier={3.4} heightMultiplier={1.6} bgColor="#d3d3d3" />
-    <LoaderStripe widthMultiplier={6} heightMultiplier={1.2} bgColor="#d3d3d3" /></> : 
+    <><LoaderStripe style={{width:"45%"}} heightMultiplier={1.6} bgColor="#d3d3d3" />
+    <LoaderStripe style={{width:"54%"}} heightMultiplier={1.2} bgColor="#d3d3d3" /></> : 
     <><h3 className="display-4">`${props.userData.first_name} ${props.userData.last_name}`</h3>
     <p className="lead">displayPermissions(props.userData.permissions) + " since " + date.toLocaleDateString()</p></>}
     <hr className="my-4" />
@@ -66,3 +53,16 @@ export default function ProfileView(props) {
 
   return userProvided;
 };
+
+function displayPermissions(permissionId) {
+  switch (permissionId) {
+    case 1:
+      return "Author";
+    case 2:
+      return "Moderator";
+    case 3:
+      return "Administrator";
+  }
+
+  return "Member";
+}
