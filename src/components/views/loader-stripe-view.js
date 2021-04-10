801892e6eb4e8348e8b949f87d0430749eccdc39
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-10 07:51:49 +0200

    Some small changes to the loader stripe

--- src/components/views/loader-stripe-view.js
+++ src/components/views/loader-stripe-view.js
@@ -2,18 +2,18 @@ import PropTypes from 'prop-types';
 
 export default function LoaderStripe(props) {
   const multipliedWidth = 5 * ('widthMultiplier' in props ? props.widthMultiplier : 1);
-
   const multipliedHeight = 1.7 * ('heightMultiplier' in props ? props.heightMultiplier : 1);
 
-  
   const style = { 
     width: (multipliedWidth + "em"),
     height: (multipliedHeight + "em"),
     backgroundColor: ('bgColor' in props ? props.bgColor : "#eeeeee")
   };
 
+  'style' in props && Object.assign(style, props.style); 
+
   return (
-    <div style={style} className="mt-3" />
+    <div style={style} className={'className' in props ? `mt-3 ${props.className}` : "mt-3"} />
   );
 };
 
