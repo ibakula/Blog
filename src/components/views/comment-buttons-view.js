2b57806db9b9c84d95bc2b60cad63d6186d6d9aa
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-25 21:39:35 +0200

    Just some styling changes

--- src/components/views/comment-buttons-view.js
+++ src/components/views/comment-buttons-view.js
@@ -1,9 +1,11 @@
+import style from './comment-buttons.module.css'
+
 export default function CommentButtonsView(props) {
   return (
     <>
-      {'showPreviousList' in props.item && <a className="text-primary" onClick={props.item.showPreviousList}>Show newer</a>}
+      {'showPreviousList' in props.item && <a className={`${style.cursor} text-primary`} onClick={props.item.showPreviousList}>Show newer</a>}
       {('showBoth' in props.item && props.item.showBoth) && <span className="ml-2 mr-2">|</span>}
-      {'showNextList' in props.item && <a className="text-primary" onClick={props.item.showNextList}>Show older</a>}
+      {'showNextList' in props.item && <a className={`${style.cursor} text-primary`} onClick={props.item.showNextList}>Show older</a>}
     </>
   );
 };
