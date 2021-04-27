653d0d3391978559ce69286588dbeb2fdb9d41fd
Ivan Bakula <ivan.bakula@outlook.com>
2021-04-27 21:32:22 +0200

    Login component code

--- src/components/containers/login-container.js
+++ src/components/containers/login-container.js
@@ -1,12 +1,48 @@
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import LoginView from '../views/login-view';
 import PopularSectionContainer from './popular-articles-container';
-import apiUtility from '../../api/api-utility';
+import * as api from '../../api/session-api';
 
 class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.formRef = createRef();
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = { success: null };
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+
+    const data = {
+      email: "",
+      password: ""
+    };
+
+    const inputs = this.formRef.current.querySelectorAll('input[id^="login"]');
+    data.email = inputs[0].value;
+    data.password = inputs[1].value;
+
+    api.login(data)
+    .then(data => {
+      if (data.result.search(/success/i) != -1) {
+        this.setState({ success: true, reason: data.reason });
+      }
+      else {
+        this.setState({ success: false, reason: data.reason });
+      }
+    })
+    .catch(error => {
+      this.setState({ success: false, reason: "Unexpected error has occured." });
+    });
+  }
+
   render() {
     return (
-      <LoginView>
+      <LoginView success={this.state.success} 
+        reason={this.state.reason} 
+        onLogin={this.handleLogin} 
+        formRef={this.formRef}>
         <PopularSectionContainer />
       </LoginView>
     );
--- src/components/views/login-view.js
+++ src/components/views/login-view.js
@@ -1,12 +1,14 @@
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button, Alert } from 'react-bootstrap';
 import style from './form-rules.module.css';
+import { Redirect } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function LoginView(props) {
   return (      
     <Row noGutters={true} className="mt-sm-5 mt-3 pl-sm-5 pl-3">
       <Col md={8}>
         <h3>User log-in:</h3>
-        <Form className="ml-3 pr-3">
+        <Form onSubmit={props.onLogin} ref={props.formRef} className="ml-3 pr-3">
           <Form.Group controlId="loginFormEmail">
             <Form.Label>User E-mail address:</Form.Label>
             <Form.Control className={style.rectifyFormCtrlItem} type="email" placeholder="E-mail address" required />
@@ -17,6 +19,12 @@ export default function LoginView(props) {
           </Form.Group>
           <Button variant="primary">Log in</Button>
         </Form>
+        {
+          props.success == false && <Alert variant="danger" className="p-4 mt-4 mr-3">Failed to login<br />Reason: {props.reason}</Alert>
+        }
+        {
+          props.success == true && <Alert variant="success" className="p-4 mt-4 mr-3">You have loggedd in, redirecting...<DeferredRedirect /></Alert>
+        }
       </Col>
       <Col md={4} className="mt-md-0 mt-4">
         {props.children} 
@@ -24,3 +32,11 @@ export default function LoginView(props) {
     </Row>
   );
 };
+
+function DeferredRedirect() {
+  const [ redirect, setRedirect ] = useState(false);
+  setTimeout(() => { setRedirect(true) }, 5000);
+  return (
+    redirect && <Redirect to="/" />
+  );
+}
