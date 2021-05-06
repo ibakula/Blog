82e01e56e1dfd508a95637df33b4a37f55e08d84
Ivan Bakula <ivan.bakula@outlook.com>
2021-05-06 21:32:06 +0200

    Navigation api: functions code added

--- src/api/navigation-api.js
+++ src/api/navigation-api.js
@@ -1,7 +1,25 @@
-import * as searchActions from '../actions/search-actions';
+import * as utility from './api-utility';
+import * as actions from '../actions/search-actions';
 import store from '../store';
 
-export function searchForTerm(term) {
-  /* ToDo: Finish implementation */
-  searchActions.initSearch();
+export function getTermsCount(sectionName, term) {
+  return utility.getDataForContainerType(`/api`, `${sectionName}/search`, '/1/count')
+  .then(({ data }) => {
+    return data.count;
+  })
+  .catch(error => {
+    return Promise.reject(error);
+  });
+};
+
+export function searchForTerm(sectionName, term, fromId, resultsCountLimit, currentListItemCount) {
+  return utility.postData(`/api/${sectionName}/search/${fromId}/${resultsCountLimit}`, term)
+  .then(({ data }) => {
+    store.dispatch(actions.getSearchResultsSuccess(data, currentListItemCount));
+    return data;
+  })
+  .catch(error => {
+    // store.dispatch(actions.getSearchResultsFail());
+    return Promise.reject(error);
+  });
 };
