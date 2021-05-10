d9302067469e1b5464acd1dd9e90e66d5880d058
Ivan Bakula <ivan.bakula@outlook.com>
2021-05-10 19:15:22 +0200

    Subscription functional

--- src/components/containers/subscription-container.js
+++ src/components/containers/subscription-container.js
@@ -1,3 +1,38 @@
+import { Component, createRef } from 'react';
 import SubscriptionView from '../views/subscription-view';
+import * as api from '../../api/api-utility';
 
-export default SubscriptionView;
+class SubscriptionContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: null };
+    this.inputRef = createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      email: this.inputRef.current.value
+    };
+
+    api.postData('http://127.0.0.1:80/api/subscription', data)
+    .then(response => {
+      this.setState({ results: response.data });
+    })
+    .catch(error => {
+      this.setState({ results: { result: 'Failed!', reason: 'Technical issue during your request has occured. E.g. network connection down?' }})
+    });
+  }
+
+  render() {
+    return (
+      <SubscriptionView inputRef={this.inputRef} 
+        onSubscribe={this.handleSubmit} 
+        results={this.state.results} />
+    );
+  }
+};
+
+export default SubscriptionContainer;
--- src/components/views/subscription-view.js
+++ src/components/views/subscription-view.js
@@ -3,18 +3,34 @@ import style from './form-rules.module.css';
 import { Row, Col, Button, Form } from 'react-bootstrap';
 
 export default function SubscriptionView(props) {
+  let elements = (<Form onSubmit={props.onSubscribe}>
+    <Form.Group controlId="subscriptionFormEmail">
+      <Form.Label className="lead">E-mail address:</Form.Label>
+      <Form.Control ref={props.inputRef} type="email" placeholder="E-mail" className={style.rectifyFormCtrlItem} required />
+    </Form.Group>
+    <Button type="submit" variant="primary">Subscribe!</Button>
+  </Form>);
+
+  if (props.results != null) {
+    if (props.results.result.search(/success/i) != -1) {
+      elements = (<>
+        <p class="text-success lead">You have successfully subscribed!</p>
+      </>);
+    }
+    else {
+      elements = (<>
+        <p class="text-danger lead">Your request has failed.</p>
+        <p class="text-danger lead">{props.results.reason}</p>
+      </>);
+    }
+  }
+
   return (
     <section className="w-100 mt-5 p-4 bg-light" height="300px">
       <Row className="justify-content-center">
         <Col xs={"auto"}>
           <h3 className="display-5">Subscribe to our content:</h3>
-          <Form>
-            <Form.Group controlId="subscriptionFormEmail">
-              <Form.Label className="lead">E-mail address:</Form.Label>
-              <Form.Control type="email" placeholder="E-mail" className={style.rectifyFormCtrlItem} required />
-            </Form.Group>
-            <Button variant="primary">Subscribe!</Button>
-          </Form>
+          {elements}
         </Col>
       </Row>
     </section>
